5b152d67c6955b1855639772c4440561a49da1fc
artem-kobelev <Artem_Kobeliev@epam.com>
2014-04-03 17:48:43 +0300

    Задачи на дженерики

---
+++ task2/Orange.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package generics.task2;
+
+/**
+ *
+ * @author Artem_Kobeliev
+ */
+public class Orange extends Fruit{
+    
+}
